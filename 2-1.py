4ea37a7fd13168a9d0cfaff44ec35ff659a477af
handa_tatsumi <tatsu@handa-tatsuminoMacBook-Pro.local>
2019-01-17 02:05:42 +0900

    first commit

---
+++ 2-1.py
@@ -0,0 +1,15 @@
+#%%
+var="test"
+print(var)
+
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+train = np.loadtxt('test_data/click.csv', delimiter=',', skiprows=1)
+train_x = train[:,0]
+train_y = train[:,1]
+
+plt.plot(train_x, train_y, 'o')
+plt.show()
